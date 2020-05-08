41fe8dda82378f44a393b7a4b4ecce61ea461c84
Swarnalatha Natarajan <swna2675@colorado.edu>
2020-05-08 16:26:50 -0600

    First go project

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World")
+
+	var name = "Swarna"
+	fmt.Println(name)
+
+	var age = 22
+	fmt.Println(age)
+
+	fmt.Printf("%T", age)
+
+	var isCool = true
+	fmt.Printf("%T", isCool)
+
+}
